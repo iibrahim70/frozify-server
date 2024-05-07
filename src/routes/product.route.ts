fcde0abb249190231d6bacc0da8c34d8b982db19
iibrahim70 <iibrahiim.dev@gmail.com>
2024-05-07 22:10:19 +0600

    added  query builder

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { ProductControllers } from '../controllers/product.controller';
+import validateRequest from '../middlewares/validateRequest';
+import productValidationSchema from '../interfaces/product.interface';
 
 const router = Router();
 
-router.post('/', ProductControllers.addProduct);
+router.post(
+  '/',
+  validateRequest(productValidationSchema),
+  ProductControllers.addProduct,
+);
 router.get('/', ProductControllers.getAllProducts);
 router.get('/:id', ProductControllers.getSingleProduct);
 router.patch('/:id', ProductControllers.updateProduct);
