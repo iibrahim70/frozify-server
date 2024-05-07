10535e8c24bb6518a3df0f32b92d1c530458210a
iibrahim70 <iibrahiim.dev@gmail.com>
2024-05-07 19:32:27 +0600

    added product validation using zod

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -1,15 +1,43 @@
-interface IProduct {
-  productName: string;
-  productTitle: string;
-  brandName: string;
-  price: number;
-  discountPercent: number;
-  flashSale: boolean;
-  images: string[];
-  inStock: number;
-  category: string;
-  subCategory: string;
-  productDescription: string;
-  rating: number;
-  numberOfRatings: number;
-}
+import { z } from 'zod';
+
+const productValidationSchema = z.object({
+  body: z.object({
+    productName: z.string({
+      required_error: 'Product Name is required.',
+      invalid_type_error: 'Product Name must be a string.',
+    }),
+    productTitle: z.string({
+      required_error: 'Product Title is required.',
+      invalid_type_error: 'Product Title must be a string.',
+    }),
+    brandName: z.string({
+      required_error: 'Brand Name is required.',
+      invalid_type_error: 'Brand Name must be a string.',
+    }),
+    price: z.number({
+      required_error: 'Price is required.',
+      invalid_type_error: 'Price must be a number.',
+    }),
+    images: z.array(z.string()).nonempty({
+      message: 'At least one image is required.',
+    }),
+    inStock: z.number({
+      required_error: 'In Stock is required.',
+      invalid_type_error: 'In Stock must be a number.',
+    }),
+    category: z.string({
+      required_error: 'Category is required.',
+      invalid_type_error: 'Category must be a string.',
+    }),
+    subCategory: z.string({
+      required_error: 'Sub Category is required.',
+      invalid_type_error: 'Sub Category must be a string.',
+    }),
+    productDescription: z.string({
+      required_error: 'Product Description is required.',
+      invalid_type_error: 'Product Description must be a string.',
+    }),
+  }),
+});
+
+export default productValidationSchema;
