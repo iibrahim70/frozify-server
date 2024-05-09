836218739f7f20d11a7ad7c5091a4d9711c7f613
iibrahim70 <iibrahiim.dev@gmail.com>
2024-05-09 14:05:37 +0600

    update default route

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// application routes
-app.use('/api/v1', router);
-
 // default route
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  // Constructing server status object
+  const serverStatus = {
+    status: 'running',
+    message: 'Frozify API is operational and running smoothly.',
+    timestamp: new Date().toISOString(),
+    version: 'v1.0.1',
+    uptime: process.uptime(),
+    author: 'Ibrahim Khalil',
+    contact: {
+      email: 'iibrahiim.dev@gmail.com',
+      website: 'https://iibrahim-dev.netlify.app/',
+    },
+  };
+  res.json(serverStatus);
 });
+
+// application routes
+app.use('/api/v1', router);
 
 // global error handler
 app.use(globalErrorHandler);
